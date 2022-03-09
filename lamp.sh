b4858111e300609d1297561b651efe8b58197f06
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-03-09 22:26:42 +0500

    Create lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,21 @@
+#Install Apache
+sudo apt update
+sudo apt install apache2 -y
+
+#Allow apache from firewall
+sudo ufw app list
+sudo ufw allow in "Apache"
+sudo ufw status
+
+#Install mysql
+sudo apt install mysql-server -y
+
+
+#Install php
+sudo apt install php libapache2-mod-php php-mysql -y
+
+#Verify Installations
+ php -v
+ apache2 -v
+ sudo mysql
+
